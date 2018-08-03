1308b23505851608f8ad3e4f616cd97aa7d07570
Thomas Chun <tchun2055@gmail.com>
2018-08-03 00:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
+  //object[key] = value;
+  //return object;
+  return Object.assign({}, object, { [key]: value })
 }
