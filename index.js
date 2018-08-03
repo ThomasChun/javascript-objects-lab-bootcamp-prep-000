00597ab2725fc2c402f63a7cb358363612619d8b
Thomas Chun <tchun2055@gmail.com>
2018-08-03 00:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object.key;
-  return object;
+  // delete object.key;
+  // return object;
+  var newObject = Object.assign({}, object)
+  delete newObject.key;
+  return newObject;
 }
