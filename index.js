90b61ac3762c774b328583ae5037e993c903fa3e
Thomas Chun <tchun2055@gmail.com>
2018-08-03 00:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  //object[key] = value;
-  //return object;
   return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
 }
